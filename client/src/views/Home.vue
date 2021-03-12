0293984ca8f5c4114bc2fa93648f0c277f25ee37
OG-Gardavsky <ogardavsky@gmail.com>
2021-03-13 00:12:20 +0100

    deploy test

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePage msg="Welcome to Budget App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from "@/components/HomePage";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
