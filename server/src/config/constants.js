75f27c57109b1dbde6c3e6d11d7bdb08b342676f
OG-Gardavsky <ogardavsky@gmail.com>
2021-03-14 13:16:06 +0100

    build heroku try out

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,8 +1,8 @@
 
 //set to procces env variables in future
 const constants = {
-    jwtKey: "someRandomKey",
-    dbAdrres: "mongodb://127.0.0.1:27017/budget_app"
+    jwtKey: "t9VR35sU%aBs#9gb686",
+    dbAdrres: "mongodb+srv://budget_app:gyUJs8YbOWUxlpCI@cluster0.m9vgm.mongodb.net/budget_app?retryWrites=true&w=majority"
 }
 
 module.exports = constants;
