2a600495086d24021b4257cc683443a5e2a897b8
OG-Gardavsky <ogardavsky@gmail.com>
2021-03-14 15:02:43 +0100

    build heroku try out - prebuild vue locally

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const transactionRouter = require('./routers/transaction');
 const categoryRouter = require('./routers/category');
 
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.use(express.json());
 app.use(userRouter);
