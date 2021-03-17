1028e1535ff0d40c34f3232344f574c568ad552b
OG-Gardavsky <ogardavsky@gmail.com>
2021-03-17 18:16:18 +0100

    Headline improvments

--- client/src/App.vue
+++ client/src/App.vue
@@ -31,4 +31,21 @@ body{
     }
   }
 }
+
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+
+
 </style>
--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,32 +1,19 @@
-<template>
-    <div>
-        <h1>{{ msg }}</h1>
-    </div>
-</template>
+<!--<template>-->
+<!--    <div>-->
+<!--        <h1>{{ msg }}</h1>-->
+<!--    </div>-->
+<!--</template>-->
 
-<script>
-export default {
-    name: 'HomePage',
-    props: {
-       msg: String
-    }
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--    name: 'HomePage',-->
+<!--    props: {-->
+<!--       msg: String-->
+<!--    }-->
+<!--}-->
+<!--</script>-->
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<!--&lt;!&ndash; Add "scoped" attribute to limit CSS to this component only &ndash;&gt;-->
+<!--<style scoped lang="scss">-->
+
+<!--</style>-->
