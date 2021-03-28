8c142cff980ce3d3a3431b8eb83287425d671143
OG-Gardavsky <ogardavsky@gmail.com>
2021-03-28 17:39:31 +0200

    proof of concept bottom menu

---
+++ client/src/views/BlankPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header />
+        <md-empty-state
+            md-icon="cloud_off"
+            md-label="There is nothing yet"
+            md-description="this funkcionality will be added">
+        </md-empty-state>
+        <CustomMenu />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import CustomMenu from "@/components/CustomMenu";
+export default {
+    name: "BlankPage",
+    components: {CustomMenu, Header}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/More.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <Header />
+
+        <div id="menuItems">
+
+            <router-link to="categories">
+                <md-card md-with-hover>
+                    <md-ripple>
+                        <md-card-header>
+                            <div class="md-title">Category management</div>
+                        </md-card-header>
+                    </md-ripple>
+                </md-card>
+            </router-link>
+
+
+            <router-link to="blank" >
+                <md-card md-with-hover>
+                    <md-ripple>
+                        <md-card-header>
+                            <div class="md-title">Investments</div>
+                        </md-card-header>
+                    </md-ripple>
+                </md-card>
+            </router-link>
+
+
+            <router-link to="blank" >
+                <md-card md-with-hover>
+                    <md-ripple>
+                        <md-card-header>
+                            <div class="md-title">Debts</div>
+                        </md-card-header>
+                    </md-ripple>
+                </md-card>
+            </router-link>
+
+
+
+        </div>
+
+        <custom-menu />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import CustomMenu from "@/components/CustomMenu";
+export default {
+    name: "More",
+    components: {CustomMenu, Header}
+}
+</script>
+
+<style lang="scss" scoped>
+    router-link {
+        margin: 5px;
+    }
+</style>
