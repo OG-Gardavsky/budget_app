3b24ab5f1da934f6f26bcf03b500e25483d47995
OG-Gardavsky <ogardavsky@gmail.com>
2021-04-12 10:05:14 +0200

    divided data model for accounts

--- server/src/models/account.js
+++ server/src/models/account.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Transaction = require('./transaction');
 
+const options = { discriminatorKey: 'type' };
 
-const acountSchema = new mongoose.Schema({
+const accountSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -12,11 +13,6 @@ const acountSchema = new mongoose.Schema({
         type: String,
         required: true,
         enum: ['debit', 'credit', 'cash', 'invest']
-    },
-    initialBalance: {
-        type: Number,
-        required: true,
-        default: 0
     },
     currency: {
         type: String,
@@ -29,9 +25,11 @@ const acountSchema = new mongoose.Schema({
         required: true,
         ref: 'User'
     }
-});
+},
+    options
+);
 
-acountSchema.virtual('transactions', {
+accountSchema.virtual('transactions', {
     ref: 'Transaction',
     localField: '_id',
     foreignField: 'accountId'
@@ -42,12 +40,48 @@ acountSchema.virtual('transactions', {
 /**
  * removing transactions with account
  */
-acountSchema.pre('remove', async function (next) {
+accountSchema.pre('remove', async function (next) {
     const account = this;
     await Transaction.deleteMany({ accountId: account._id });
     next();
 });
 
-const Account = mongoose.model('Account', acountSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+
+/**
+ *
+ * @type {module:mongoose.Schema<Document, Model<any, any>, undefined>}
+ */
+const cashSchema = new mongoose.Schema({
+    initialBalance: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+},
+    options
+);
+
+Account.discriminator('cash', cashSchema);
+
+/**
+ *
+ * @type {module:mongoose.Schema<Document, Model<any, any>, undefined>}
+ */
+const debitSchema = new mongoose.Schema({
+        initialBalance: {
+            type: Number,
+            required: true,
+            default: 0
+        }
+    },
+    options
+);
+Account.discriminator('debit', debitSchema);
+
+
+
+
 
 module.exports = Account;
