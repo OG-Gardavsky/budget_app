bb4f55e48bb0608c456289eb4c13f972ab4a99ad
OG-Gardavsky <ogardavsky@gmail.com>
2021-04-14 17:46:50 +0200

    debt page update + delete of account posible

--- client/src/views/Debts.vue
+++ client/src/views/Debts.vue
@@ -14,38 +14,70 @@
 
 
 
-        <md-card
-            :key="account.accountId" v-for="account in accountsBalance"
-            class="debtAccount" md-with-hover
-            v-if="selectedAccount === null"
-            @click="selectedAccount = account"
+        <div :key="account.accountId" v-for="account in accountsBalance"
+             v-if="selectedAccount === null"
+             @click="selectedAccount = account ; selectedAccount.balance = account.balance; showTransactionsOfSpecificAccount(account) "
         >
-            <md-content>
+            <md-card
+                class="debtAccount" md-with-hover
+            >
+                <md-content>
 
-                <md-card-header>
-                    <div class="md-title">
-                        <span>{{account.name}}</span>
-                        <span> <span v-if="account.balance > 0">+ </span> {{account.balance}} {{account.currency}}</span>
-                    </div>
-                </md-card-header>
-            </md-content>
-        </md-card>
+                    <md-card-header>
+                        <div class="md-title">
+                            <span>{{account.name}}</span>
+                            <span> <span v-if="account.balance > 0">+ </span> {{account.balance}} {{account.currency}}</span>
+                        </div>
+                    </md-card-header>
+                </md-content>
+            </md-card>
+        </div>
 
 
 
 
-        <div v-if="selectedAccount !== null">
-            <md-card >
+        <div v-if="selectedAccount !== null" id="accountDetails">
+
+            <md-card id="accountCard">
                 <md-content >
-
-                    <md-button class="md-icon-button  md-primary" md-menu-trigger >
+                    <md-button class="md-icon-button  md-primary"  @click="selectedAccount = null ; selectedAccountTransactions = null" >
                         <md-icon>arrow_back_ios</md-icon>
                     </md-button>
-
+                    <div class="md-title">{{selectedAccount.name}}</div>
+                    <div class="md-title">{{selectedAccount.balance}} {{selectedAccount.currency}}</div>
                 </md-content>
             </md-card>
 
-            <h1>je to tu</h1>
+
+            <md-button class="md-button md-primary md-raised" @click="deleteAccount">delete account</md-button>
+            <md-button class="md-button md-primary md-raised" @click="showUpdateAccountDialog = true">edit account</md-button>
+
+            <UpdateAccount v-if="showUpdateAccountDialog === true"
+                           :show-update-account-dialog="showUpdateAccountDialog"
+                           :account-to-update="selectedAccount"
+                           @on-save="refresh" @on-closeModal="showUpdateAccountDialog = false"
+            />
+
+
+
+
+
+            <md-card :key="transaction._id" v-for="transaction in selectedAccountTransactions" md-with-hover>
+
+                <md-content class="transactionContent">
+
+                    <div class="baseInfo">
+                        <div class="md-title">{{transaction.subtype}}</div>
+                        <div class="md-title">{{transaction.amount}} {{transaction.currency}}</div>
+                    </div>
+
+                    <div class="md-subhead">neco</div>
+
+                </md-content>
+
+            </md-card>
+
+
         </div>
 
 
@@ -60,22 +92,60 @@
 <script>
 import Header from "@/components/Header";
 import CustomMenu from "@/components/CustomMenu";
+import UpdateAccount from "@/components/UpdateAccount";
 export default {
 name: "Debts",
-    components: {CustomMenu, Header},
+    components: {UpdateAccount, CustomMenu, Header},
     data() {
         return {
+            confirmationDialog: true,
+
+            showUpdateAccountDialog: false,
+
             accountsBalance: null,
             totalDebtsSum: null,
-            selectedAccount: null
+
+            selectedAccount: null,
+            selectedAccountTransactions: null
         }
     },
     methods: {
         async refresh() {
             this.accountsBalance = this.showBalanceOfAccounts('debt');
             this.totalDebtsSum = await this.getTotalBalanceByAccType('debt');
-        },
 
+
+        },
+        async showTransactionsOfSpecificAccount(account) {
+            const res = await fetch('api/accounts/id:' + account._id.toString() + '/transactions', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+                }
+            });
+            this.selectedAccountTransactions = await res.json();
+        },
+        async deleteAccount(){
+            const answer = window.confirm('Are you sure you want to delete account: ' + this.selectedAccount.name + '? Include all transactions');
+
+            if (!answer) {
+                return;
+            }
+
+            const res = await fetch('api/accounts/id:' + this.selectedAccount._id, {
+                method: 'Delete',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+                }
+            });
+
+            if (res.status !== 200){
+                return this.displayCustomError('Error during deleting of ' + this.selectedAccount.name);
+            }
+
+            this.selectedAccount = null;
+            await this.refresh();
+        },
     },
     created() {
         this.refresh();
@@ -84,6 +154,40 @@ name: "Debts",
 </script>
 
 <style scoped lang="scss">
+
+#accountDetails {
+    max-width: 60%;
+    margin: 20px auto;
+
+    @media screen and (max-width: 560px) {
+        max-width: 90%;
+    }
+    .md-card {
+        padding: 10px;
+        margin: 10px 0;
+    }
+
+    #accountCard {
+
+        .md-content {
+            padding: 10px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+    }
+
+    .transactionContent {
+        .baseInfo {
+            padding: 10px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+    }
+
+
+}
 
 #totalBalanceCard {
     align-items: center;
