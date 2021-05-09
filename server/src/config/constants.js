d2046ac980fd19f2a6a8420d8a30a34f95861242
OG-Gardavsky <ogardavsky@gmail.com>
2021-05-09 20:15:50 +0200

    deleting constants in configuration

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,10 +1,10 @@
 const constants = {
-    dbAdrres: 'mongodb+srv://budget_app:gyUJs8YbOWUxlpCI@cluster0.m9vgm.mongodb.net/budget_app?retryWrites=true&w=majority',
+    dbAdrres: '',
     bcryptSaltRounds: 10,
-    jwtKey: 't9VR35sU%aBs#9gb686',
-    currencyApiKey : '9482cf901cbc017dda7c',
-    sendGridApiKey: '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',
-    emailAdressForSending: 'ogardavsky@seznam.cz'
+    jwtKey: '',
+    currencyApiKey : '',
+    sendGridApiKey: '',
+    emailAdressForSending: ''
 }
 
 module.exports = constants;
